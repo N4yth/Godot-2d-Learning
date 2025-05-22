495730a00f83f28b8574eb751e08f95155f50ec8
N4yth <nathan.dupuis31@gmail.com>
2025-05-22 09:23:28 +0200

    add : create a little map with wall that can collide with the player

--- player.cs
+++ player.cs
@@ -2,10 +2,10 @@ using Godot;
 using System;
 using static Godot.GD;
 
-public partial class player : Area2D
+public partial class player : CharacterBody2D
 {
     [Export]
-    public int Speed = 400; // How fast the player will move (pixels/sec).
+    public int Speed = 115; // How fast the player will move (pixels/sec).
 
     private Vector2 _screenSize; // Size of the game window.
 
@@ -17,6 +17,7 @@ public partial class player : Area2D
     public override void _PhysicsProcess(double delta)
     {
         var velocity = new Vector2(); // The player's movement vector.
+        var animatedSprite = GetNode<AnimatedSprite2D>("AnimatedSprite2D");
 
         if (Input.IsActionPressed("ui_right"))
         {
@@ -38,7 +39,16 @@ public partial class player : Area2D
             velocity.Y -= 1;
         }
 
-        var animatedSprite = GetNode<AnimatedSprite2D>("AnimatedSprite2D");
+        if (Input.IsActionPressed("ui_Shift"))
+        {
+            Speed = 200;
+            animatedSprite.SpeedScale = 3;
+        }
+        else
+        {
+            Speed = 115;
+            animatedSprite.SpeedScale = 2;
+        }
 
         if (velocity.Length() > 0)
         {
@@ -51,8 +61,8 @@ public partial class player : Area2D
 
         Position += velocity * (float)delta;
         Position = new Vector2(
-            x: Mathf.Clamp(Position.X, 0, _screenSize.X),
-            y: Mathf.Clamp(Position.Y, 0, _screenSize.Y)
+            x: Position.X,
+            y: Position.Y
         );
 
         if (velocity.X > 0)
@@ -61,21 +71,32 @@ public partial class player : Area2D
             animatedSprite.FlipH = true;
             animatedSprite.FlipV = velocity.X < 0;
         }
-        else if(velocity.X < 0)
+        else if (velocity.X < 0)
         {
             animatedSprite.Animation = "right";
+            animatedSprite.FlipH = true;
             animatedSprite.FlipV = velocity.X > 0;
         }
-        else if(velocity.Y < 0)
+        else if (velocity.Y < 0)
         {
             animatedSprite.Animation = "up";
             animatedSprite.FlipH = true;
             animatedSprite.FlipV = velocity.Y > 0;
         }
-        else if(velocity.Y > 0)
+        else if (velocity.Y > 0)
         {
             animatedSprite.Animation = "down";
             animatedSprite.FlipV = velocity.Y < 0;
         }
-    }
+
+        // Using MoveAndSlide.
+        Velocity = velocity; // <-- propriété héritée de CharacterBody2D
+        MoveAndSlide();
+
+        for (int i = 0; i < GetSlideCollisionCount(); i++)
+        {
+            var collision = GetSlideCollision(i);
+        }
+    }  
 }
+
